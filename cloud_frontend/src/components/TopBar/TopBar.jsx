0643a88274cf19a6091b9cf975b559af12c839c6
Abel George Stanley <abel@kalayil.com>
2025-05-25 22:34:00 +0530

    just few minor adjustments

--- cloud_frontend/src/components/TopBar/TopBar.jsx
+++ cloud_frontend/src/components/TopBar/TopBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, TextField, Button, Box, InputAdornment } from '@mui/material';
+import { AppBar, Toolbar, TextField, Button, Box, InputAdornment, Typography } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import FileUploadIcon from '@mui/icons-material/FileUpload';
 import SearchIcon from '@mui/icons-material/Search';
@@ -7,44 +7,60 @@ import SearchIcon from '@mui/icons-material/Search';
 const TopBar = ({ onCreateFolder, onUploadFile, currentFolder }) => {
   return (
     <AppBar position="static" color="default" elevation={0}>
-      <Toolbar>
+      <Toolbar sx={{ justifyContent: 'space-between' }}> {/* Ensure space between elements */}
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, flexGrow: 1 }}>
-          <Button 
-            variant="contained" 
+          <Button
+            variant="contained"
             startIcon={<AddIcon />}
             onClick={onCreateFolder}
           >
             New Folder
           </Button>
-          
-          <Button 
-            variant="outlined" 
+
+          <Button
+            variant="outlined"
             startIcon={<FileUploadIcon />}
             component="label"
           >
             Upload
-            <input 
-              type="file" 
-              hidden 
-              multiple 
+            <input
+              type="file"
+              hidden
+              multiple
               onChange={(e) => onUploadFile(e.target.files)}
             />
           </Button>
         </Box>
-        
-        <TextField
-          variant="outlined"
-          size="small"
-          placeholder="Search..."
-          sx={{ minWidth: 300 }}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon color="action" />
-              </InputAdornment>
-            ),
-          }}
-        />
+
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}> {/* Wrapper for search and project name */}
+          <TextField
+            variant="outlined"
+            size="small"
+            placeholder="Search..."
+            sx={{ minWidth: 300 }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon color="action" />
+                </InputAdornment>
+              ),
+            }}
+          />
+          {/* Project Name */}
+          <Typography
+            variant="h6" // Adjust variant for desired size
+            component="div"
+            sx={{
+              fontWeight: 'bold',
+              letterSpacing: 1,
+              color: 'primary.main', // Use primary color from theme
+              //textTransform: 'uppercase', // Optional: make it uppercase
+              ml: 2 // Add some left margin
+            }}
+          >
+            STAN drive
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
